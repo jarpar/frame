6d6383e71b62915d1ef24011bd9e748730c85dc7
jarpar <partyka.jaroslaw@gmail.com>
2020-03-02 10:38:20 +0100

    refactored

---
+++ src/centrowanieRamki/Main.java
@@ -0,0 +1,15 @@
+package centrowanieRamki;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+    public Main() {
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
