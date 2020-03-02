40162dd18ead92e22fa025195057ae415bd51164
jarpar <partyka.jaroslaw@gmail.com>
2020-03-02 14:12:06 +0100

    refactored

--- src/ukladGraficzny/Main.java
+++ src/ukladGraficzny/Main.java
@@ -8,7 +8,32 @@ public class Main extends JFrame {
         super("Layout");
         this.setBounds(300, 320, 300, 200);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        initComponents();
+
     }
+
+    public void initComponents() {
+
+        center = new JButton("Center");
+        up = new JButton("Up");
+        down = new JButton("Down");
+        left = new JButton("Left");
+        right = new JButton("Right");
+
+        Container container = this.getContentPane();
+
+        container.add(center, BorderLayout.CENTER);
+        container.add(up, BorderLayout.PAGE_START);
+        container.add(down, BorderLayout.PAGE_END);
+        container.add(left, BorderLayout.LINE_START);
+        container.add(right, BorderLayout.LINE_END);
+    }
+
+    JButton center;
+    JButton up;
+    JButton down;
+    JButton left;
+    JButton right;
 
     public static void main(String[] args) {
         new Main().setVisible(true);
