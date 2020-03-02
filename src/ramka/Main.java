dd0c9897eefc1969a1402d3c3c1976b28bb059c1
jarpar <partyka.jaroslaw@gmail.com>
2020-03-02 09:10:42 +0100

    refactored

--- src/ramka/Main.java
+++ src/ramka/Main.java
@@ -1,8 +1,13 @@
 package ramka;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
+    public Main() {
+        JFrame frame = new JFrame();
+    }
+
     public static void main(String[] args) {
 
     }
