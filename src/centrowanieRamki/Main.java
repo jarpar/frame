ee119ddbec9038792ed36337579084ce3380e802
jarpar <partyka.jaroslaw@gmail.com>
2020-03-02 13:50:35 +0100

    refactored

--- src/centrowanieRamki/Main.java
+++ src/centrowanieRamki/Main.java
@@ -5,11 +5,17 @@ import java.awt.*;
 
 public class Main extends JFrame {
     public Main() {
-
+        int width = Toolkit.getDefaultToolkit().getScreenSize().width;
+        int height = Toolkit.getDefaultToolkit().getScreenSize().height;
+        //System.out.println(width + " x " + height);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(width / 2, height / 2);
+        this.setLocation(width / 4, height / 4);
 
     }
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        new Main().setVisible(true);
     }
 }
