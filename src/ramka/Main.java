56a1a91fac38b75f2707fade8cba1883fd5f713c
jarpar <partyka.jaroslaw@gmail.com>
2020-03-02 09:27:05 +0100

    refactored

--- src/ramka/Main.java
+++ src/ramka/Main.java
@@ -8,7 +8,9 @@ public class Main {
         JFrame frame = new JFrame("Tytuł ramki");
         //frame.setTitle("Tytuł ramki");
         frame.setSize(640, 480);
-
+        //frame.setSize(new Dimension(640, 480));
+        //frame.setLocation(100, 50);
+        frame.setLocation(new Point(100, 50));
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
