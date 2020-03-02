e6dc4987feb9dd9cbb62af79c4328eb3d4e8c1c5
jarpar <partyka.jaroslaw@gmail.com>
2020-03-02 09:13:20 +0100

    refactored

--- src/ramka/Main.java
+++ src/ramka/Main.java
@@ -6,9 +6,12 @@ import java.awt.*;
 public class Main {
     public Main() {
         JFrame frame = new JFrame();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-
+        new Main();
     }
 }
